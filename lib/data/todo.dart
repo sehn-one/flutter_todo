b6df5c37225b8eb449416605402f4a77c24e36cd
lee_se_hyun <sehn_86@naver.com>
2021-12-31 02:02:37 +0900

    data class 오류 수정

--- lib/data/todo.dart
+++ lib/data/todo.dart
@@ -1,9 +1,9 @@
-class Todo{
+class Todo {
   late String title;
   late String memo;
   late String category;
   late int color;
   late int done;
   late int date;
-  Todo(this.title,this.memo,this.category,this.color,this.done, this.date)
+  Todo(this.title, this.memo, this.category, this.color, this.done, this.date);
 }
