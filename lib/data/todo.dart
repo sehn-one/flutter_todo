28334eb448dbbf56405804c242c99c90f62289b0
lee_se_hyun <sehn_86@naver.com>
2021-12-31 01:52:41 +0900

    todo 데이터 설계, 기존적인 util 함수 추가

---
+++ lib/data/todo.dart
@@ -0,0 +1,9 @@
+class Todo{
+  late String title;
+  late String memo;
+  late String category;
+  late int color;
+  late int done;
+  late int date;
+  Todo(this.title,this.memo,this.category,this.color,this.done, this.date)
+}
---
+++ lib/data/util.dart
@@ -0,0 +1,22 @@
+class Util {
+  static int getFormatTime(DateTime date) {
+    // date.month 7월 => 07(x) 7(o)
+    // 이것을 맞추기 위해 makeTwoDigit을 사용
+    return int.parse("${date.year}${makeTwoDigit(date.month)}${date.day}");
+  }
+
+  static DateTime numToDateTime(int date){
+    String _d = date.toString();
+    int year = int.parse(_d.substring(0,4);
+    int month = int.parse(_d.substring(4,2);
+    int day = int.parse(_d.substring(6,2);
+    return DateTime(year,month,day)
+  }
+
+  static String makeTwoDigit(int num) {
+    //두자리 공간을 마련하고 빈공간은 0으로 채운다
+    //  2 => 02
+    // 10 => 10
+    return num.toString().padLeft(2, "0");
+  }
+}
